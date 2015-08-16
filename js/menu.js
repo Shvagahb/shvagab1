b1c579bd6df1eb7e2b15d1bc3d16341b3710f59b
freesaga <jaymjames@me.com>
2015-08-15 21:58:30 -0400

    cm

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,12 @@
 var idasorbust=window.location.href;
+
 if(idasorbust == "http://shvagahb.github.io/resume.html"){
+	// console.log("doge")
+}
+
+if(idasorbust == "http://shvagahb.github.io"){
+	document.getElememtById("dogedown").style.bottom="140px";
+	document.getElememtById("dogedown").style.left="50%";
 	console.log("doge")
 }
 // console.log(idasorbust);	
