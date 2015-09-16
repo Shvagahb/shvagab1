3fb6a9498b9867dcca35019ea8cf8a726ec6d14c
shvagahb <biancaaidass@gmail.com>
2015-09-15 23:10:08 -0400

    length check

--- js/main.js
+++ js/main.js
@@ -6,8 +6,23 @@ $(function(){ /* to make sure the script runs after page load */
         $(this).parents('.item').find('.more_text').toggle(); /* show the .more_text span */
 
     });
+
+	if(window.innerWidth < 662) {
+		for(i=3; i<11; i++) {
+			if(i>2 && i<9 && x[i].innerHTML.indexOf(breakString) == -1) {
+				console.log("if2nd");
+				x[i].innerHTML += "<br>";
+			}
+		}
+	}else {
+		console.log("else ran");
+		for(i=3; i<11; i++) {
+			console.log("forelseran");
+			x[i].innerHTML.replace(breakString, " "); //fix this to remove br's
+		}
+	}
+    
 var x = document.getElementsByClassName("h6a");
-console.log(x);
 var breakString = "<br>";
 
 window.onresize = function(event) {
@@ -22,7 +37,7 @@ window.onresize = function(event) {
 		console.log("else ran");
 		for(i=3; i<11; i++) {
 			console.log("forelseran");
-			x[i].innerHTML.replace(breakString, " ");
+			x[i].innerHTML.replace(breakString, " "); //fix this to remove br's
 		}
 	}
 			// console.log(x); 
