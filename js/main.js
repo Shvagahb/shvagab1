b27e7e140e40d9720283035dfc08c130e875e267
shvagahb <biancaaidass@gmail.com>
2015-09-15 21:49:31 -0400

    length check

--- js/main.js
+++ js/main.js
@@ -6,18 +6,19 @@ $(function(){ /* to make sure the script runs after page load */
         $(this).parents('.item').find('.more_text').toggle(); /* show the .more_text span */
 
     });
+var x = document.getElementsByClassName("h6a");
 
 window.onresize = function(event) {
    
 	// if(window.innerWidth < 441) {
-	// 	var x = document.getElementsByClassName("h6a");
-	// 	for(i=0; x.length<11; i++) {
+
+	// 	for(i=4; i<11; i++) {
 	// 		if(i>3) {
 	// 			x[i].innerHTML = text;
 	// 		}
 	// 	}
 	// }
-			console.log("snoop drizzlely"); 
+			console.log(x); 
    
 };
 
